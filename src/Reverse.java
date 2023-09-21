8b2df48965072ee80c5f92e51b619aa90c4c044c
ask0908 <askdfjaslk@naver.com>
2023-09-21 20:55:08 +0900

    고정 용량 스택 전까지

---
+++ src/Reverse.java
@@ -0,0 +1,12 @@
+public class Reverse {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        while (!StdIn.isEmpty()) {
+            stack.push(StdIn.readInt());
+        }
+
+        for (int i : stack) {
+            StdOut.println(i);
+        }
+    }
+}
